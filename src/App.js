06518207a07a1f4724dc3131a2ae93138d0829f6
kjd95110 <std95110@naver.com>
2019-12-10 13:26:01 +0900

    6강 Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme =>({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  table:{
+    minWidth:1080
+  }
+})
 
 const customers=[
   {
@@ -33,24 +52,32 @@ const customers=[
 class App extends Component {
 ///* 맵을 사용할때는 고유한 키값을 줘야한다.
   render() {
+    const { classes } = this.props;  // 새로 추가된  style이 적용될수 있도록 classes 변수를 하나만들어서 
     return (     
-      <div>
-
-        
-      { customers.map(c => {
-      return(
-        <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}  ></Customer>
-      );
-    })  
-  
-  }
-
-
-      </div>      
+      <Paper className={classes.root}>        
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          { customers.map(c => {
+              return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}  ></Customer>  );
+            })  
+          }
+         </TableBody>
+        </Table>      
+      </Paper>      
     )
   }
 }
-export default App;
+export default withStyles(styles) (App);
 
 
 
