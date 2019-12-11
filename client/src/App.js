5aa2f6e29870c70e15c8a69a03bfcbc8e159aad6
kjd95110 <std95110@naver.com>
2019-12-11 12:09:42 +0900

    9강 React라이프사이클이해및  API로딩처리구현

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import { withStyles } from '@material-ui/core/styles';
 
 
@@ -18,6 +20,9 @@ const styles = theme =>({
   },
   table:{
     minWidth:1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 })
 
@@ -28,21 +33,29 @@ class App extends Component {
 ///* 맵을 사용할때는 고유한 키값을 줘야한다.
 
 state = {
-  customers: ""
+  customers: "",
+  completed: 0 
 }
 
 componentDidMount(){
-  this.callApi()
+  this.timer = setInterval(this.progress, 20);
+   this.callApi()
   
-  //받아와서...
-  .then(res => this.setState({customers: res}))
-  .catch(err => console.log(err));
+   //componentDidMount()가 callApi를 호출(비동기적으로)하고 이결과를 받아와서...상태가 변화(setState)되고  React에서 이를감지하고 뷰가 재구성되기 [ render() ]때문에  화면에 보여지게되는것이다.
+   .then(res => this.setState({customers: res}))
+   .catch(err => console.log(err));
 }
 
 callApi = async () => {
   const response = await fetch('/api/customers');
   const body = await response.json();
   return body;
+}
+
+progress = () => {
+  const { completed } = this.state;
+  this.setState({ completed : completed >= 100 ? 0 : completed + 1 });
+
 }
 
   render() {
@@ -63,7 +76,11 @@ callApi = async () => {
           <TableBody>          
             {this.state.customers ? this.state.customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}  ></Customer>  );
             }) : "" }
-          
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}></CircularProgress>
+              </TableCell>
+            </TableRow>
          </TableBody>
         </Table>      
       </Paper>      
