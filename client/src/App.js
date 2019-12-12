a2e36afb0c9b9b871ea843a51f641c2ac1710fed
kjd95110 <std95110@naver.com>
2019-12-12 13:17:40 +0900

    17강AppBar 및 웹폰트적용디자인개편

--- client/src/App.js
+++ client/src/App.js
@@ -12,23 +12,99 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 const styles = theme =>({
   root:{
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto'
+    minWidth:1080       
   },
-  table:{
-    minWidth:1080
+  menu:{
+    marginTop:15,
+    marginBottom:15,
+    display:'flex',
+    justifyContent:'center'
+  },
+  paper:{
+    marginLeft:18,
+    marginRight:18
+  },
+  grow:{
+    flexGrow:1,
+  },
+  TableHead:{
+    fontSize:'1.3rem'
   },
   progress: {
     margin: theme.spacing.unit * 2
+  } ,
+
+  menuButton: {
+    marginLeft:-12,
+    marginRight: 20,
+  },
+  toolbar: {
+    minHeight: 128,
+    alignItems: 'flex-start',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
+  title: {
+    display:'none',
+    [theme.breakpoints.up('sm')]:{
+      display:'block',
+    },
+    search:{
+      position:'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white,0.15),
+      '&:hover':{
+        backgroundColor:fade(theme.palette.common.white,0.25),
+      },
+      marginLeft:0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]:{
+        marginLeft: theme.spacing.unit,
+        whdth: 'auto',
+      }
+    } ,  
+  },
+  searchIcon:{
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iinputRoot:{
+    color:'inherit',
+    width:'100%',
+  },
+  inputInput:{
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]:{
+      width:120,
+      '&:focus':{
+        width:200,
+      },
+    },
   }
-})
 
-
-
+});
 
 class App extends Component {
 ///* 맵을 사용할때는 고유한 키값을 줘야한다.
@@ -85,19 +161,43 @@ progress = () => {
 
   render() {
     const { classes } = this.props;  // 새로 추가된  style이 적용될수 있도록 classes 변수를 하나만들어서 
+    const cellList = ["번호","프로필이미지","이름","생년월일","성별","직업","설정"]
     return (
-      <div>     
-      <Paper className={classes.root}>        
+      <div className={classes.root}>
+          <AppBar position="static">
+              <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="open drawer" >
+                  <MenuIcon />
+                </IconButton>
+                <Typography className={classes.title} variant="h6" noWrap>
+                  고객 관리 시스템
+                </Typography>
+                <div className={classes.grow}></div>
+                  <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase placeholder="검색하기"
+                   classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}                    
+                  />
+                </div>
+              </Toolbar>
+            </AppBar>
+
+          <div className={classes.menu}>
+              <CustomerAdd stateRefresh={this.stateRefresh}></CustomerAdd>
+            </div>
+      <Paper className={classes.paper} >        
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-              <TableCell>설정</TableCell>
+              {cellList.map(c => {
+                return <TableCell className={classes.TableHead}>{c}</TableCell>
+              })              
+            }
             </TableRow>
           </TableHead>
           <TableBody>          
@@ -111,7 +211,7 @@ progress = () => {
          </TableBody>
         </Table>      
       </Paper>
-      <CustomerAdd stateRefresh={this.stateRefresh}></CustomerAdd>
+     
       </div>
     )
   }
